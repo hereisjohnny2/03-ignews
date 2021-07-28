2816a895a90bc5be81536a2632186c8c4762a41c
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-07-27 21:36:02 -0300

    Redirencionando usuário que não possui um conta ativa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
+import { session } from "next-auth/client";
 
 export default NextAuth({
   providers: [
@@ -12,6 +13,41 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscriptionByUserRef"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("userByEmail"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index("subscriptionByStatus"),
+                "active"
+              )]
+            )
+          )
+        );  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
       
